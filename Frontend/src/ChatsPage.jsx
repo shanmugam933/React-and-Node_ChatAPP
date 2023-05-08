cb355e1d528eff8cc8519a4be448f0b54502d9ae
Shanmugam <53436674+shanmugam933@users.noreply.github.com>
2023-05-08 23:16:57 +0530

    Changed_UI

--- Frontend/src/ChatsPage.jsx
+++ Frontend/src/ChatsPage.jsx
@@ -1,15 +1,34 @@
-import { MultiChatSocket,MultiChatWindow,useMultiChatLogic } from "react-chat-engine-advanced";
+// import { MultiChatSocket,MultiChatWindow,useMultiChatLogic } from "react-chat-engine-advanced";
 
+
+// const ChatsPage = (props) => {
+//     const chatProps = useMultiChatLogic(
+//             'b7d57e61-990b-4337-aae1-1c8e3ae8f631',
+//             props.user.username,
+//             props.user.secret
+//         );
+//     return (<div style={{height:'100vh'}}>
+//         <MultiChatSocket {...chatProps} />
+//         <MultiChatWindow {...chatProps} style={{height:'100vh'}}/>
+//     </div>)
+//   };
+
+import { PrettyChatWindow } from "react-chat-engine-pretty";
 
 const ChatsPage = (props) => {
-    const chatProps = useMultiChatLogic(
-            'b7d57e61-990b-4337-aae1-1c8e3ae8f631',
-            props.user.username,
-            props.user.secret
-        );
-    return (<div style={{height:'100vh'}}>
-        <MultiChatSocket {...chatProps} />
-        <MultiChatWindow {...chatProps} style={{height:'100vh'}}/>
-    </div>)
-  };
+  return (
+    <div className="background">
+      <PrettyChatWindow
+        projectId='b7d57e61-990b-4337-aae1-1c8e3ae8f631'
+        username={props.user.username}
+        secret={props.user.secret}
+      />
+    </div>
+  );
+};
+
+
+
   export default ChatsPage;
+
+
